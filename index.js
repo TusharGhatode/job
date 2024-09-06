38146fef02758684678e1a35f62006f116a901da
Tushar Ghatode <123729340+TusharGhatode@users.noreply.github.com>
2024-09-06 10:05:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const registration = require('./routes/registration')
+const profile = require('./routes/profile')
+const jobsPost = require('./routes/jobspost')
+const email = require('./routes/email')
+const apply = require('./routes/apply')
+const filter = require('./routes/filter')
+
+
+
+
+
+require('dotenv').config(); 
+const PORT = process.env.PORT || 5000;
+const app = express();
+require('./config/db')
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,DELETE',
+    credentials: true,
+    
+}))
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(registration)
+app.use(profile)
+app.use(jobsPost)
+app.use(email)
+app.use(apply)
+app.use(filter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
